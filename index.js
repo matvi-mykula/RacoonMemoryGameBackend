cf140d8f6a5d08d1af59b93421f820585be19504
matvimykula <matt.pronchick@gmail.com>
2022-11-21 22:59:48 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+var cors = require('cors');
+
+const app = express();
+const port = 3005;
+app.use(cors());
+app.use(express.json());
+
+// Import the mongoose module
+const mongoose = require('mongoose');
+const Score = require('./models/score');
+
+// Set up default mongoose connection
+const mongoDB =
+  'mongodb+srv://matvi_mykula:this1works@cluster0.o1l2bk9.mongodb.net/MemoryGame?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Get the default connection
+const db = mongoose.connection;
+
+// Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.post('/api/highscores', (req, res) => {
+  console.log(req.body);
+  //   res.send('Hello World!');
+  const score = new Score({
+    score: req.body.aScore,
+    name: req.body.aName,
+  });
+  score.save((err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  console.log('score saved');
+  //   getHighScores();
+  res.json({ message: 'success' });
+});
+
+app.get('/api/scorelist', async (req, res) => {
+  return Score.find()
+    .sort({ score: -1 })
+    .limit(10)
+    .exec(function (err, entries) {
+      console.log(entries);
+      return res.end(JSON.stringify(entries));
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ models/score.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+// const { DateTime } = require('luxon');
+
+const Schema = mongoose.Schema;
+const HighScoreSchema = new Schema({
+  name: { type: String, maxLength: 100 },
+  //   date: { type: Date },
+  score: { type: Number },
+});
+
+//can create a virtual for date_formatted and for url
+
+module.exports = mongoose.model('HighScore', HighScoreSchema);
